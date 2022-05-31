986b36ab78f9f6797e87f06f4b5ab9c29fe7fe48
Ornella Tchinda <ornella.tchinda@yahoo.fr>
2022-05-31 11:59:52 -0400

    Add edge cases to coding challenges from june 1st

--- CC June 1st/app.js
+++ CC June 1st/app.js
@@ -4,13 +4,20 @@
 
 // use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
 
-// mission 2:
+function helloWorld() {
+    // mission 2:
 
-// use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+    // use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+    var str = "Hello World"
 
-// mission 3:
+    // mission 3:
 
-// type the console.log() in the next line(don't forget to put the str in the brackets).
+    // type the console.log() in the next line(don't forget to put the str in the brackets).
+
+    console.log(str)
+}
+
+
 
 
 function helloWorld() {
